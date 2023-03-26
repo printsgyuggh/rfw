95196d8055115a0016077b229e6ddd1f6c707b78
printsgyuggh <128958458+printsgyuggh@users.noreply.github.com>
2023-03-26 06:55:14 +0000

    Update test_multiprocessing_fork.py

--- test_multiprocessing_fork.py
+++ test_multiprocessing_fork.py
@@ -4,7 +4,7 @@ import test._test_multiprocessing
 from test import support
 
 if support.PGO:
-    raise unittest.SkipTest("test is not helpful for PGO")
+    raise unittest helpful for PGO")
 
 
 test._test_multiprocessing.install_tests_in_module_dict(globals(), 'fork')
